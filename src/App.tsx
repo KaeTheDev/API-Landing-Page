56ade9a8991129e9df0f01da8d057ae06f23f17b
KaeTheDev <kaethedev@gmail.com>
2026-01-02 11:39:57 -0500

    Hero component with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Navbar from "./components/Navbar/Navbar"
+import Hero from "./components/Hero/Hero"
+// import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <>
       {/* <h2 className="text-3xl text-blue-500">KaeTheDev is the GREATEST!</h2> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,34 @@
+export default function Hero() {
+    return(
+        <section className="hero">
+        <div className="max-w-7xl mx-auto my-4 px-4">
+          <div className="flex flex-col-reverse md:flex-row items-center">
+            
+            {/* Text content */}
+            <div className="w-full md:w-1/2 text-center md:text-left mt-4 md:mt-0">
+              <h1 className="font-bold text-3xl md:text-5xl mt-3">
+                More than just shorter links
+              </h1>
+              <p className="text-gray-500 mt-4">
+                Build your brand’s recognition and get detailed insights on
+                how your links are performing.
+              </p>
+              <button className="mt-6 px-6 py-3 border bg-teal-400 hover:bg-teal-600 hover:text-white text-white font-bold rounded transition">
+                Get Started
+              </button>
+            </div>
+      
+            {/* Image */}
+            <div className="w-full md:w-1/2 flex justify-center md:justify-end mb-4 md:mb-0">
+              <img
+                src="images/illustration-working.svg"
+                className="max-w-full h-auto"
+                alt="Person working on computer illustration"
+              />
+            </div>
+      
+          </div>
+        </div>
+      </section>      
+    )
+}
